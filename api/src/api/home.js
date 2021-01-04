e1e5ed0438393459accbd639adfccdc2cea78773
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-04 16:17:16 -0600

    Initial commit

---
+++ api/src/api/home.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { errorWrap } = require('../middleware');
+
+const Home = require('../models/home');
+
+router.get(
+  '/',
+  errorWrap(async (req, res) => {
+    // MongoDB connection
+    // const homeText = await Home.findOne();
+    const homeText = "You've connected the database! Isn't it so beautiful???";
+    res.status(200).json({
+      message: `Successfully returned home text`,
+      success: true,
+      result: homeText,
+    });
+  }),
+);
+
+module.exports = router;
