ef7728a25c9947541e1fd095058aba7afa375a09
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-01-05 12:48:06 -0600

    FORMAT: Removed unused imports

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Navbar = () => {
 
@@ -8,10 +7,6 @@ const Navbar = () => {
 
         </div>
     )
-}
-
-Navbar.propTypes = {
-
 }
 
 export default Navbar;
--- client/src/pages/Member.js
+++ client/src/pages/Member.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Member = ({ }) => {
-
-    React.useEffect(() => {
-
-    }, []);
 
     return (
         <div>
 
         </div>
     )
-}
-
-Member.propTypes = {
-
 }
 
 export default Member;
