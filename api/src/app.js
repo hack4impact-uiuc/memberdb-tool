1f9c02928a8580ec8870fa567506d7a66c13f109
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-08 19:25:57 -0600

    Enable connection to Mongo

--- api/src/app.js
+++ api/src/app.js
@@ -20,17 +20,16 @@ if (process.env.NODE_ENV != 'production') {
 }
 
 // CONNECTION TO MONGO
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// mongoose.connect(process.env.MONGO_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.Promise = global.Promise;
 
-// mongoose.Promise = global.Promise;
-
-// mongoose.connection
-//   .once('open', () => console.log('Connected to MongoLab instance.'))
-//   .on('error', error => console.log('Error connecting to MongoLab:', error));
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(helmet());
 app.use(cors());
