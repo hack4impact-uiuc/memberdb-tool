521dfa15897983402b39a829133982c42a8a1fba
mlw6 <mattwalowski@gmail.com>
2021-01-12 11:38:56 -0600

    Use VERCEL_URL variable

--- client/src/utils/apiWrapper.js
+++ client/src/utils/apiWrapper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:9000/api';
+const BASE_URL = process.env.REACT_APP_VERCEL_URL ? `https://${process.env.REACT_APP_VERCEL_URL}/api` : 'http://localhost:9000/api';
 
 /**
  * Returns a sample API response to demonstrate a working backend
