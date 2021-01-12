ea19c9ff02ed92c9ac38bdf1088529ae100691d8
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-12 12:59:49 -0600

    Add protected route as an example

--- api/src/api/home.js
+++ api/src/api/home.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { errorWrap } = require('../middleware');
+const { requireRegistered } = require('../middleware/auth');
 
 const Home = require('../models/home');
 
 router.get(
   '/',
+  errorWrap(async (req, res) => {
+    const home = await Home.findOne();
+    res.status(200).json({
+      message: `Successfully returned home text`,
+      success: true,
+      result: home.text,
+    });
+  }),
+);
+
+router.get(
+  '/registeredOnly',
+  [requireRegistered],
   errorWrap(async (req, res) => {
     const home = await Home.findOne();
     res.status(200).json({
