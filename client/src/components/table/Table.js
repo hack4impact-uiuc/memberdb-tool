577b705b20df5757532f06207d6d63a9723c3e6e
Amit Sawhney <66758185+amit-sawhney@users.noreply.github.com>
2021-01-12 22:47:08 -0600

    Frontend Auth (#13)
    * FEAT: basic structure of navbar
    
    * FEAT: added login feature to view any page
    
    * REMOVE: incorrent dependency location
    
    * FEAT: add login connected to UNTESTED endpoint
    
    * PAGE: added 404 Not Found Page
    
    * LINT: added prop-types and styling
    
    * REMOVE: deleted 404 page
    
    * DEPENDENCY: added ag-grid
    
    * STYLING: Updated Profile dropdown CSS
    
    * STYLING: changed Login Page styling
    
    * FEAT: added proper headers for ag-grid
    
    * LINT: fixes some lint issues
    
    * DOCUMENATION: added comments to components
    
    * LINT: fixed documentation styling
    
    * LINT: fixed prop-types
    
    * PR FEEDBACK: resolves @alicesf2 comments
    
    * PR FEEDBACK: resolves @mattwalo32 comments

---
+++ client/src/components/table/Table.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+import '../../css/Table.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const Table = () => {
+  const [columnDefs] = useState([
+    { headerName: 'Name', field: 'name', width: 100, sortable: true },
+    { headerName: 'Email', field: 'email', width: 180, sortable: true },
+    { headerName: 'Phone #', field: 'phone', width: 160, sortable: true },
+    { headerName: 'NetID', field: 'netid', width: 120, sortable: true },
+    { headerName: 'UIN', field: 'uin', width: 120, sortable: true },
+    { headerName: 'Major', field: 'major', width: 120, sortable: true },
+    {
+      headerName: 'Birth Date',
+      field: 'birthdate',
+      width: 120,
+      sortable: true,
+    },
+    { headerName: 'Github', field: 'github', width: 140, sortable: true },
+    { headerName: 'Snapchat', field: 'snapchat', width: 140, sortable: true },
+    { headerName: 'Instagram', field: 'instagram', width: 140, sortable: true },
+    { headerName: 'Grad Sem/Yr', field: 'grad', width: 190, sortable: true },
+    {
+      headerName: 'Generation',
+      field: 'generation',
+      width: 120,
+      sortable: true,
+    },
+    { headerName: 'Location', field: 'location', width: 120, sortable: true },
+    { headerName: 'Role', field: 'role', width: 160, sortable: true },
+    { headerName: 'Level', field: 'level', width: 120, sortable: true },
+    { headerName: 'Status', field: 'status', width: 120, sortable: true },
+  ]);
+  const [rowData] = useState([
+    {
+      name: 'Amit',
+      email: 'sawhney4@illinois.edu',
+      phone: '(309) 838-5466',
+      netid: 'sawhney4',
+      uin: 659426199,
+      major: 'CS + Math',
+      birthdate: '02/14/2002',
+      github: 'amit-sawhney',
+      snapchat: 'amit_sawhney02',
+      instagram: 'amit__02',
+      grad: 'Spring 2024 (Freshman)',
+      generation: 'Fall 2020',
+      location: 'On-campus',
+      role: 'Software Developer',
+      level: 'Member',
+      status: 'Active',
+    },
+  ]);
+  return (
+    <div className="ag-theme-alpine table-wrapper">
+      <AgGridReact columnDefs={columnDefs} rowData={rowData}></AgGridReact>
+    </div>
+  );
+};
+
+export default Table;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import googleIcon from '../assets/google-logo.png';
+
+import '../css/Login.css';
+
+/**
+ * Displays the login page over everything else
+ * @param {func} login connects to backend to start a users current session.
+ */
+const Login = ({ login }) => {
+  return (
+    <div className="login-wrapper">
+      <div className="login-card">
+        <h2>Member Database Login</h2>
+        <button type="button" onClick={login} className="login-btn">
+          <img className="google-icon" src={googleIcon} alt="Google Icon" />
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default Login;
