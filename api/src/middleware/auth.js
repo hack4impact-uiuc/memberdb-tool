89554f4d95ba2f8476c2d8d0fd7744ef8278f0b9
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-14 10:34:23 -0600

    Add more auth utils

--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -1,7 +1,25 @@
 const { levelEnum } = require('../models/member');
 
+const allLevels = [
+  levelEnum.TBD,
+  levelEnum.MEMBER,
+  levelEnum.LEAD,
+  levelEnum.DIRECTOR,
+  levelEnum.ADMIN,
+];
+
+// Boolean checks for usage inside route
+const hasLevel = (user, levels) => user && levels.includes(user.level);
+
+const isRegistered = (user) => hasLevel(user, allLevels);
+const isMember = (user) => hasLevel(user, allLevels.slice(1));
+const isLead = (user) => hasLevel(user, allLevels.slice(2));
+const isDirector = (user) => hasLevel(user, allLevels.slice(3));
+const isAdmin = (user) => hasLevel(user, allLevels.slice(4));
+
+// Middleware for authentication
 const requireLevel = (levels) => (req, res, next) => {
-  if (!req.user || !levels.includes(req.user.level)) {
+  if (!hasLevel(req.user, levels)) {
     return res.status(401).json({
       success: false,
       message: 'Unauthorized',
@@ -11,26 +29,19 @@ const requireLevel = (levels) => (req, res, next) => {
   }
 };
 
-const requireRegistered = requireLevel(Object.values(levelEnum));
-
-const requireMember = requireLevel([
-  levelEnum.ADMIN,
-  levelEnum.DIRECTOR,
-  levelEnum.LEAD,
-  levelEnum.MEMBER,
-]);
-
-const requireLead = requireLevel([
-  levelEnum.ADMIN,
-  levelEnum.DIRECTOR,
-  levelEnum.LEAD,
-]);
-
-const requireDirector = requireLevel([levelEnum.ADMIN, levelEnum.DIRECTOR]);
-
-const requireAdmin = requireLevel([levelEnum.ADMIN]);
+const requireRegistered = requireLevel(allLevels);
+const requireMember = requireLevel(allLevels.slice(1));
+const requireLead = requireLevel(allLevels.slice(2));
+const requireDirector = requireLevel(allLevels.slice(3));
+const requireAdmin = requireLevel(allLevels.slice(4));
 
 module.exports = {
+  hasLevel,
+  isRegistered,
+  isMember,
+  isLead,
+  isDirector,
+  isAdmin,
   requireLevel,
   requireRegistered,
   requireMember,
