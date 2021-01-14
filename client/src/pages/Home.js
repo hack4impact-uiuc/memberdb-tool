71af36857d5987311c46f955030f81f095b1fca5
Amit Sawhney <66758185+amit-sawhney@users.noreply.github.com>
2021-01-14 00:20:15 -0600

    Frontend-Backend Auth connection (#22)
    * Redirct to backend login api
    
    * Use CORS in dev with proper auth settings
    
    * FEAT: removes auth state and connects sess. to api
    
    * LINT: fixed styling and prop-type errors
    
    * Ran prettier
    
    Co-authored-by: Jeffrey Tang <jeffrey.tang80@gmail.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,32 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Table from '../components/table/Table';
 
-import { getSampleResponse } from '../utils/apiWrapper';
-
 import '../css/Home.css';
 
-const Home = () => {
-  const [text, setText] = useState('You did not run local API!');
-
-  useEffect(() => {
-    const populateText = async () => {
-      const resp = await getSampleResponse();
-      if (!resp.error) setText(resp.data.result);
-    };
-
-    populateText();
-  }, []);
-
-  return (
-    <div>
-      <div style={{ textAlign: 'center' }}>
-        <h1>API Status Below</h1>
-        <p>{text}</p>
-      </div>
-      <Table />
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <Table />
+  </div>
+);
 
 export default Home;
---
+++ client/src/utils/apiUrls.js
@@ -0,0 +1,9 @@
+const BACKEND_BASE_URL = process.env.REACT_APP_VERCEL_URL
+  ? `https://${process.env.REACT_APP_VERCEL_URL}/api`
+  : 'http://localhost:9000/api';
+
+const FRONTEND_BASE_URL = process.env.REACT_APP_VERCEL_URL
+  ? `https://${process.env.REACT_APP_VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export { BACKEND_BASE_URL, FRONTEND_BASE_URL };
