41a7973caae50704db5c3070ab2e6e1f7a28b6a4
Alice Fang <alicesf2@illinois.edu>
2021-01-13 23:21:14 -0600

    added custom sorting for date-related fields

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -6,6 +6,34 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const Table = () => {
+  // Sort Birth Dates in ascending order (earlier dates first)   
+  const dateComparator = (value1, value2) => {
+    const date1 = new Date(value1)
+    const date2 = new Date(value2)
+    return date1 - date2
+  }
+
+  // Sort Grad Year in ascending order (earlier grad years first)
+  const semesterComparator = (value1, value2) => {
+    const sem1 = value1.split(" ")[0]
+    const year1 = value1.split(" ")[1]
+    const sem2 = value2.split(" ")[0]
+    const year2 = value2.split(" ")[1]
+    if (year1 < year2) {
+      return -1
+    } else if (year1 > year2) {
+      return 1
+    } else {
+      if (sem1 === "Fall" && sem2 === "Spring") {
+        return 1
+      } else if (sem1 === "Spring" && sem2 === "Fall") {
+        return -1
+      } else {
+        return 0
+      }
+    }
+  }
+
   const [columnDefs] = useState([
     { headerName: 'Name', field: 'name', width: 100, sortable: true },
     { headerName: 'Email', field: 'email', width: 180, sortable: true },
@@ -24,6 +52,7 @@ const Table = () => {
       field: 'birthdate',
       width: 120,
       sortable: true,
+      comparator: dateComparator
     },
     { headerName: 'Github', field: 'github', width: 140, sortable: true },
     { headerName: 'Snapchat', field: 'snapchat', width: 140, sortable: true },
@@ -34,6 +63,7 @@ const Table = () => {
       width: 190,
       sortable: true,
       filter: true,
+      comparator: semesterComparator
     },
     {
       headerName: 'Generation',
@@ -41,6 +71,7 @@ const Table = () => {
       width: 120,
       sortable: true,
       filter: true,
+      comparator: semesterComparator
     },
     {
       headerName: 'Location',
@@ -71,6 +102,7 @@ const Table = () => {
       filter: true,
     },
   ]);
+
   const [rowData] = useState([
     {
       name: 'Amit',
@@ -107,6 +139,24 @@ const Table = () => {
       role: 'Tech Lead',
       level: 'Member',
       status: 'Active',
+    },
+    {
+      name: 'Jefffrey',
+      email: 'alicesf2@illinois.edu',
+      phone: '(123)-456-7891',
+      netid: 'alicesf2',
+      uin: 123456789,
+      major: 'CS',
+      birthdate: '01/02/1999',
+      github: 'alicesf2',
+      snapchat: 'alicesf2',
+      instagram: 'alicesf2',
+      grad: 'Spring 2020',
+      generation: 'Spring 2024',
+      location: 'Remote',
+      role: 'Tech Lead',
+      level: 'Member',
+      status: 'Hiatus',
     },
   ]);
   return (
