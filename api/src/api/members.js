f3fed7156f0f775165cec279b3eb8eed694704f6
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-14 19:22:37 -0600

    Members endpoints with GET, POST, and PUT (#21)
    * Create basic endpoint skeleton
    
    * get endpoint 500 error
    
    * Fix routing
    
    * Get member endpoint to work
    
    * Return more fields from endpoint
    
    * Return object id from endpoint
    
    * Add filterSensitiveInfo util
    
    * Give all non-required Member fields a default of null
    
    * Move passport-setup to utils
    
    * Remove console.log
    
    * Add members endpoints
    
    * Fix filterSensitiveInfo util
    
    * Use passport-setup.js in utils
    
    * Use auth endpoint
    
    * Move /api/auth/user to /api/members/current
    
    * Add omit fields to GET /:memberId
    
    * Change current user endpoint in frontend
    
    * Add more auth utils
    
    * Use isDirector from module in members routes
    
    * Change level default in model back to TBD
    
    * Add level priority explanation
    
    * Add detailed permission checking in members endpoint
    
    * Fix difference function
    
    * Fix typos in user-utils
    
    * Add _id as neverEditable field
    
    * Fix allFields not being checked correctly
    
    * Filter viewable fields when returning from PUT
    
    Co-authored-by: mlw6 <mattwalowski@gmail.com>
    Co-authored-by: ishaansharma <ishaan02@hotmail.com>

---
+++ api/src/api/members.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const router = express.Router();
+const Member = require('./../models/member');
+const errorWrap = require('../middleware/errorWrap');
+const { requireRegistered, requireDirector } = require('../middleware/auth');
+const {
+  allFields,
+  filterViewableFields,
+  getViewableFields,
+  getEditableFields,
+} = require('../utils/user-utils');
+
+const validateMemberQuery = (req, res, next) => {
+  // Middleware that verifies that fields to be inserted/updated actually
+  // exist in the Member schema, and that user has permission to update them
+  const editableFields = getEditableFields(req.user, req.params.memberId);
+
+  for (const field in req.body) {
+    if (!allFields.includes(field)) {
+      return res.status(400).json({
+        success: false,
+        message: `No such field ${field} in member`,
+      });
+    }
+
+    if (!editableFields.includes(field)) {
+      return res.status(401).json({
+        success: false,
+        message: `Cannot update protected field ${field} in member`,
+      });
+    }
+  }
+  next();
+};
+
+router.get(
+  '/current',
+  errorWrap(async (req, res) => {
+    res.json({
+      success: true,
+      result: req.user ? filterViewableFields(req.user, req.user) : null,
+    });
+  }),
+);
+
+router.get(
+  '/',
+  requireRegistered,
+  errorWrap(async (req, res) => {
+    const members = await Member.find({});
+    const filteredMembers = members.map((member) =>
+      filterViewableFields(req.user, member),
+    );
+
+    res.json({
+      success: true,
+      result: filteredMembers,
+    });
+  }),
+);
+
+router.post(
+  '/',
+  requireDirector,
+  validateMemberQuery,
+  errorWrap(async (req, res) => {
+    const member = await Member.create(req.body);
+    return res.status(200).json({
+      success: true,
+      result: member,
+    });
+  }),
+);
+
+router.get(
+  '/:memberId',
+  requireRegistered,
+  errorWrap(async (req, res) => {
+    const member = await Member.findById(req.params.memberId);
+    if (!member) {
+      return res.status(404).json({
+        success: false,
+        message: 'Member not found with id',
+      });
+    }
+
+    res.json({
+      success: true,
+      result: filterViewableFields(req.user, member),
+    });
+  }),
+);
+
+router.get(
+  '/:memberId/permissions',
+  requireRegistered,
+  errorWrap(async (req, res) => {
+    res.json({
+      success: true,
+      result: {
+        view: getViewableFields(req.user, req.params.memberId),
+        edit: getEditableFields(req.user, req.params.memberId),
+      },
+    });
+  }),
+);
+
+router.put(
+  '/:memberId',
+  requireRegistered,
+  validateMemberQuery,
+  errorWrap(async (req, res) => {
+    const updatedMember = await Member.findByIdAndUpdate(
+      req.params.memberId,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedMember) {
+      return res.status(404).json({
+        success: false,
+        message: 'Member not found with id',
+      });
+    }
+
+    res.json({
+      success: true,
+      message: 'Member updated',
+      result: filterViewableFields(req.user, updatedMember),
+    });
+  }),
+);
+
+module.exports = router;
