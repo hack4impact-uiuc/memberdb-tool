d4a43e7a02bfcdd1357746c59f4d4f26f094ee04
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-16 21:01:37 -0600

    Consolidate .env files (#28)
    * Consolidate envs and clean up app.js
    
    * Fix yarn test not using .env

--- api/src/app.js
+++ api/src/app.js
@@ -1,48 +1,25 @@
 const createError = require('http-errors');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const express = require('express');
-const fs = require('fs');
 const helmet = require('helmet');
 const logger = require('morgan');
-const mongoose = require('mongoose');
-const path = require('path');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const apiRoutes = require('./api');
-
-const app = express();
 const { errorHandler } = require('./middleware');
 
 const environment = process.env.NODE_ENV || 'dev';
-if (environment != 'production') {
-  dotenv.config({
-    path: path.resolve(__dirname, `../config/${environment}.env`),
-  });
-}
+const app = express();
 
-// CONNECTION TO MONGO
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.Promise = global.Promise;
-
-mongoose.connection
-  .once('open', () => console.log('Connected to MongoLab instance.'))
-  .on('error', (error) => console.log('Error connecting to MongoLab:', error));
-
+// HTTPS, CORS, bodyParser
 app.use(helmet());
 if (environment != 'production') {
   // For authentication to work on CORS, we must specify a non-wildcard origin
   // and allow credentials (cookies)
   app.use(cors({ origin: /localhost:\d{4}/, credentials: true }));
 }
-
 app.use(logger('dev'));
-
 app.use(bodyParser.json({ limit: '2.1mb' }));
 app.use(bodyParser.urlencoded({ limit: '2.1mb', extended: false }));
 
@@ -56,15 +33,17 @@ app.use(
   }),
 );
 
+// Mongo setup
+require('./utils/mongo-setup');
+
 // Passport setup
 require('./utils/passport-setup');
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Routes
 app.use('/api', apiRoutes);
-
 app.get('/', (req, res) => res.json('API working!'));
-
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(function (req, res, next) {
