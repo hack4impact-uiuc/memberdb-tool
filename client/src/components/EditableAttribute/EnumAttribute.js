b47e8a0d9a79da437fe83802c31d14f8bee4733e
mlw6 <mattwalowski@gmail.com>
2021-01-18 09:00:11 -0600

    Fix default enum attribute option

--- client/src/components/EditableAttribute/EnumAttribute.js
+++ client/src/components/EditableAttribute/EnumAttribute.js
@@ -5,7 +5,7 @@ import Select from 'react-select';
 const defaultDropdownOption = { label: '', value: '' };
 
 const EnumAttribute = ({
-  value = defaultDropdownOption,
+  value = defaultDropdownOption.value,
   valueOptions = [],
   attributeLabel = '',
   isDisabled = false,
