7eca8491609dc7737fccf40a112808d4df951e8a
ishaansharma <ishaan02@hotmail.com>
2021-01-19 11:56:45 -0800

    added api call to get all members

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -2,22 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 
 import columnDefs from '../../utils/tableHelpers';
-import mockMemberData from '../../utils/mockMemberData';
+import { getMembers } from '../../utils/apiWrapper';
 import '../../css/Table.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const Table = () => {
-  const [members] = useState(mockMemberData);
-  const getMembers = async () => {
-    // TODO:
-    // await api call to retrieve all members
-    // set members state variable to returned data
-  };
-
+  const [members, setMembers] = useState([]);
   useEffect(() => {
-    getMembers();
+    const getAllMembers = async () => {
+      const allMembers = await getMembers()
+      setMembers(allMembers)
+    };
+    getAllMembers();
   }, []);
+
+
 
   return (
     <div className="ag-theme-alpine table-wrapper">
