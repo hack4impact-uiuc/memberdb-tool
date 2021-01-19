e28bd740d57f4157fb1eed0428f82d195f07bfa3
Jeffrey Tang <jeffrey.tang80@gmail.com>
2021-01-19 16:33:05 -0600

    Update editable fields (#43)

--- api/src/utils/user-utils.js
+++ api/src/utils/user-utils.js
@@ -9,10 +9,21 @@ const neverViewableFields = ['oauthID', '__v'];
 // Fields that are never editable
 const neverEditableFields = ['_id', 'oauthID', '__v'];
 // Fields that non-directors cannot view for other users
-// TODO: omit notes fields once they are added to the DB
-const nonViewableFields = ['level', 'areDuesPaid'];
+const nonViewableFields = ['areDuesPaid'];
 // Fields that non-directors cannot edit for themselves
-const nonEditableFields = ['level', 'areDuesPaid'];
+const nonEditableFields = [
+  'email',
+  'level',
+  'areDuesPaid',
+  'gradYear',
+  'gradSemester',
+  'classStanding',
+  'generationYear',
+  'generationSemester',
+  'location',
+  'role',
+  'status',
+];
 
 const getViewableFields = (currentUser, memberId) => {
   const viewableFields = difference(allFields, neverViewableFields);
