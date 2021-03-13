24d8a0db2b7b0a7f0d49246714ee1130762f6f29
Leundai <batmanlup2@gmail.com>
2021-03-13 12:03:03 -0600

    add momentjs and sorting/cohorts dates

--- client/src/utils/tableHelpers.js
+++ client/src/utils/tableHelpers.js
@@ -1,25 +1,54 @@
+import moment from 'moment';
+
+const birthDateFormat = 'MM/DD/YYYY';
+
 // Sort Birth Dates in ascending order (earlier dates first)
 const dateComparator = (value1, value2) => {
-  const date1 = new Date(value1);
-  const date2 = new Date(value2);
-  return date1 - date2;
+  const date1 = moment(value1, birthDateFormat);
+  const date2 = moment(value2, birthDateFormat);
+  return date1.diff(date2);
 };
 
 // Sort Grad Year && Generation in ascending order (earlier cohorts first)
 const semesterComparator = (value1, value2) => {
-  const [sem1, year1] = value1.split(' ');
-  const [sem2, year2] = value2.split(' ');
+  const [sem1, year1] = value1
+    .split(' ')
+    .map((val) => (parseInt(val, 10) ? Number(val) : val));
+  const [sem2, year2] = value2
+    .split(' ')
+    .map((val) => (parseInt(val, 10) ? Number(val) : val));
   if (year1 < year2) return -1;
   if (year1 > year2) return 1;
-  if (sem1 === 'Fall' && sem2 === 'Spring') return 1;
-  if (sem1 === 'Spring' && sem2 === 'Fall') return -1;
+  if (sem1 === 'FALL' && sem2 === 'SPRING') return 1;
+  if (sem1 === 'SPRING' && sem2 === 'FALL') return -1;
   return 0;
+};
+
+// Combine both Semester and Year into a single column
+const graduationGetter = ({ data }) => {
+  if (data.gradSemester === 'TBD') return data.gradSemester;
+  return `${data.gradSemester} ${data.gradYear}`;
+};
+
+const generationGetter = ({ data }) => {
+  if (data.generationSemester === 'TBD') return data.generationSemester;
+  return `${data.generationSemester} ${data.generationYear}`;
+};
+
+const nameGetter = ({ data }) =>
+  `${data.firstName ?? ''} ${data.lastName ?? ''}`;
+
+const birthDateGetter = ({ data }) => {
+  if (!data.birthdate) return 'Not Given';
+  const date = moment(data.birthdate).format(birthDateFormat);
+  return date;
 };
 
 export default [
   {
     headerName: 'Name',
     field: 'name',
+    valueGetter: nameGetter,
     checkboxSelection: true,
   },
   {
@@ -36,7 +65,7 @@ export default [
   },
   {
     headerName: 'UIN',
-    field: 'uin',
+    field: 'UIN',
   },
   {
     headerName: 'Major',
@@ -44,7 +73,7 @@ export default [
   },
   {
     headerName: 'Birth Date',
-    field: 'birthdate',
+    valueGetter: birthDateGetter,
     comparator: dateComparator,
   },
   {
@@ -61,12 +90,12 @@ export default [
   },
   {
     headerName: 'Grad Sem/Yr',
-    field: 'grad',
+    valueGetter: graduationGetter,
     comparator: semesterComparator,
   },
   {
     headerName: 'Generation',
-    field: 'generation',
+    valueGetter: generationGetter,
     comparator: semesterComparator,
   },
   {
