9761ae6bd90b0fb2ef81c9b43acbc3880ed26b48
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-04-03 12:58:26 -0500

    add notes routing

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,3 +1,5 @@
 export const DEFAULT = '/';
 export const MEMBER_PAGE = '/member/:memberID';
 export const LOGIN_PAGE = '/login';
+export const NOTES = '/notes';
+export const NOTE_PAGE = `/notes/:noteID`;
