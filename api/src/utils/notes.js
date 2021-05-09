99672cc8aaa5b942103c19eda9d4fd1003dbe2be
Yousef Ahmed <yousefahmed0066@gmail.com>
2021-05-09 05:08:12 -0500

    Add encryption and decryption algorithms

---
+++ api/src/utils/notes.js
@@ -0,0 +1,41 @@
+const openpgp = require('openpgp');
+const { generateEncryptionPasswords } = require('./users');
+
+// encrypt a note with AES 256 encryption
+const encryptNote = async (data) => {
+  const viewerIds = [
+    ...data.metaData.access.editableBy,
+    ...data.metaData.access.viewableBy,
+  ];
+
+  const uniqueViewerIds = [...new Set(viewerIds)];
+  const passwords = await generateEncryptionPasswords(uniqueViewerIds);
+
+  const message = await openpgp.createMessage({
+    text: data.content.toString(),
+  });
+  const encrypted = await openpgp.encrypt({
+    message,
+    passwords,
+    config: { preferredCompressionAlgorithm: openpgp.enums.compression.zlib }, // compress with zlib
+  });
+  return encrypted;
+};
+
+// decrypt a note with AES 256 encryption
+const decryptNote = async (encryptedNote, encryptionPassword) => {
+  const encryptedMessage = await openpgp.readMessage({
+    armoredMessage: encryptedNote, // parse encrypted bytes
+  });
+  const { data: decrypted } = await openpgp.decrypt({
+    message: encryptedMessage,
+    passwords: [encryptionPassword], // decrypt with password
+  });
+  console.log(decrypted);
+  return decrypted;
+};
+
+module.exports = {
+  encryptNote,
+  decryptNote,
+};
