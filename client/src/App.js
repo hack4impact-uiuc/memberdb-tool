87242bb4a24162e24ef83e1e08c677baf19305a8
Neeraj Aggarwal <hello@neerajaggarwal.com>
2021-05-27 23:45:07 -0700

    Add private routing (#91)

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import Note from './pages/Note';
 import Notes from './pages/Notes';
 import Navbar from './components/navbar/Navbar';
+import PrivateRoute from './components/routes/PrivateRoute';
 import { getUserAuth } from './utils/apiWrapper';
 
 function App() {
@@ -42,13 +43,21 @@ function App() {
             </Route>
           </Switch>
         </Route>
-        <Route path={Routes.NOTE_PAGE}>{user && <Note user={user} />}</Route>
-        <Route path={Routes.NOTES}>
-          <Notes />
-        </Route>
-        <Route path={Routes.DEFAULT}>
-          {user ? <Home user={user} /> : <Redirect to={Routes.LOGIN_PAGE} />}
-        </Route>
+        <PrivateRoute
+          path={Routes.NOTE_PAGE}
+          authed={user !== null}
+          component={<Note user={user} />}
+        />
+        <PrivateRoute
+          path={Routes.NOTES}
+          authed={user !== null}
+          component={<Notes />}
+        />
+        <PrivateRoute
+          path={Routes.DEFAULT}
+          authed={user !== null}
+          component={<Home user={user} />}
+        />
       </Switch>
     </div>
   );
---
+++ client/src/components/routes/PrivateRoute.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+import * as Routes from '../../routes';
+
+const PrivateRoute = ({ path, authed, component }) =>
+  authed ? (
+    <Route path={path} exact component={component} />
+  ) : (
+    <Redirect to={Routes.LOGIN_PAGE} />
+  );
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  authed: PropTypes.bool.isRequired,
+  component: PropTypes.object.isRequired,
+};
+
+export default PrivateRoute;
