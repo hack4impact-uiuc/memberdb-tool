131325e8f992a8a5007bff89b4e53bf59f4a5efc
Manuja DeSilva <mdesilva@bu.edu>
2021-06-10 12:26:49 -0400

    added delete route for project

---
+++ api/src/api/projects.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { requireDirector } = require('../middleware/auth');
+const errorWrap = require('../middleware/errorWrap');
+const router = express.Router();
+const Project = require("../models/project");
+
+router.delete(
+    "/:id", 
+    requireDirector,
+    errorWrap(async (req, res) => {
+        const id = req.params.id;
+        const project = await Project.findById(id);
+        if (!project) {
+            return res.status(404).json({
+                success: false,
+                message: "Project not found"
+            })
+        }
+        await Project.delete({"_id": id});
+        return res.json({
+            success: true,
+            message: "Successfully deleted project"
+        })
+    })
+
+})
