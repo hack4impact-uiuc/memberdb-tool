f7d48a8fb1d66e40d456bad3bba6cb9e73c75432
Eric Newcomer <enewcomer23@gmail.com>
2021-07-08 15:56:47 -0700

    fix prettier formatting error

--- api/src/models/chapters.js
+++ api/src/models/chapters.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 
 const statusEnum = {
-    INCOMING: "INCOMING",
-    INCUBATING: "INCUBATING",
-    MATURE: "MATURE",
-    DISSOLVED: "DISSOLVED"
-}
-
+  INCOMING: 'INCOMING',
+  INCUBATING: 'INCUBATING',
+  MATURE: 'MATURE',
+  DISSOLVED: 'DISSOLVED',
+};
 
 const Chapter = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    status: {
-      type: String,
-      enum: Object.values(statusEnum),
-      required: true
-    },
-    website: { type: String, default: null },
-    linkedin: { type: String, default: null },
-    github: { type: String, default: null },
-    notion: { type: String, default: null },
-    socialUrls: { type: [String], default: null },
-    leaders: [{type: mongoose.Schema.Types.ObjectId, ref: "Member"}]
-  });
-  
-  module.exports = mongoose.model('Chapter', Chapter);
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  status: {
+    type: String,
+    enum: Object.values(statusEnum),
+    required: true,
+  },
+  website: { type: String, default: null },
+  linkedin: { type: String, default: null },
+  github: { type: String, default: null },
+  notion: { type: String, default: null },
+  socialUrls: { type: [String], default: null },
+  leaders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
+});
+
+module.exports = mongoose.model('Chapter', Chapter);
