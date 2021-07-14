063a9161d1ace9ccbc96bce82891744c5ede5d21
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2021-07-14 12:39:52 -0400

    Migrated PRs

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use('/home', require('./home'));
 router.use('/auth', require('./auth'));
 router.use('/members', require('./members'));
+router.use('/projects', require('./projects'));
 router.use('/notes', require('./notes'));
 router.use('/chapters', require('./chapters'));
 
