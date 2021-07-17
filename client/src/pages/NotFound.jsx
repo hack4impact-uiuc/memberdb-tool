589dbc595d597133d71dfab3a8ee0fc53f3357f6
Aditya <adityajain1921@gmail.com>
2021-07-17 01:21:26 -0500

    Add NotFound page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react';
+import type { Node } from 'react';
+
+import Page from '../components/layout/Page';
+
+//import '../css/Home.css';
+
+const NotFound = (): Node => <Page title="Page Does Not Exist" />;
+
+export default NotFound;
