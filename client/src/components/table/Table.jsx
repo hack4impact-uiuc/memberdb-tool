16a2fb2085e459889c368edf833d19e22c39c453
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2021-07-28 20:14:25 -0400

    Fixed format issues

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -2,11 +2,10 @@
 import React, { useState, useEffect } from 'react';
 import type { Node } from 'react';
 import { AgGridReact } from 'ag-grid-react';
-
 import '../../css/Table.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import { Button } from "semantic-ui-react";
+import { Button } from 'semantic-ui-react';
 
 type TableProp = {
   data: Array<Object>,
@@ -17,12 +16,12 @@ type TableProp = {
 
 const Table = ({ data, columns, onRowClick, sizeToFit }: TableProp): Node => {
   const [entries, setEntries] = useState([]);
-  const [gridApi, setGridApi ] = useState(null);
+  const [gridApi, setGridApi] = useState(null);
 
   const onGridReady = (params) => {
     setGridApi(params.api);
     sizeToFit && params.api.sizeColumnsToFit();
-  }
+  };
 
   useEffect(() => {
     setEntries(data);
@@ -31,7 +30,10 @@ const Table = ({ data, columns, onRowClick, sizeToFit }: TableProp): Node => {
   return (
     <div className="ag-theme-alpine table-wrapper">
       <div className="action-container">
-        <Button size="medium" onClick={() => gridApi?.exportDataAsCsv()}> Export as CSV</Button>
+        <Button size="medium" onClick={() => gridApi?.exportDataAsCsv()}>
+          {' '}
+          Export as CSV
+        </Button>
       </div>
       <AgGridReact
         onGridReady={onGridReady}
