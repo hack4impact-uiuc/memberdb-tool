36f4f05dd0cdcb9ef2ab930091dfca36f926731c
Eric Newcomer <enewcomer23@gmail.com>
2021-08-11 11:57:00 -0700

    remove curly braces for linter

--- client/src/components/WarningModal/WarningModal.jsx
+++ client/src/components/WarningModal/WarningModal.jsx
@@ -22,33 +22,31 @@ const WarningModal = ({
   isOpen,
   noText = 'No',
   yesText = 'Yes',
-}: WarningModalProp): Node => {
-  return (
-    <Modal className="warning-modal" open={isOpen}>
-      <Header className="warning-modal-title" content={title} />
-      <Modal.Content>
-        <p className="warning-modal-description">{description}</p>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button
-          className="warning-modal-button"
-          color="red"
-          onClick={() => noAction()}
-        >
-          {noText}
-        </Button>
-        <Button
-          className="warning-modal-button"
-          color="green"
-          icon
-          labelPosition="right"
-          onClick={() => yesAction()}
-        >
-          {yesText} <Icon name="checkmark" />
-        </Button>
-      </Modal.Actions>
-    </Modal>
-  );
-};
+}: WarningModalProp): Node => (
+  <Modal className="warning-modal" open={isOpen}>
+    <Header className="warning-modal-title" content={title} />
+    <Modal.Content>
+      <p className="warning-modal-description">{description}</p>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button
+        className="warning-modal-button"
+        color="red"
+        onClick={() => noAction()}
+      >
+        {noText}
+      </Button>
+      <Button
+        className="warning-modal-button"
+        color="green"
+        icon
+        labelPosition="right"
+        onClick={() => yesAction()}
+      >
+        {yesText} <Icon name="checkmark" />
+      </Button>
+    </Modal.Actions>
+  </Modal>
+);
 
 export default WarningModal;
