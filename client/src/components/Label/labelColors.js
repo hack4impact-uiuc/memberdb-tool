f06bbe8b9c174ce697b1a5e78c3672f32cae132d
Vance Winstead <vance.winstead@gmail.com>
2021-12-06 17:02:35 -0800

    Added Label Component

---
+++ client/src/components/Label/labelColors.js
@@ -0,0 +1,12 @@
+const colors = {
+  greenBackground: '#CDFFCD',
+  greenText: '#007F00',
+  yellowBackground: '#FFFACD',
+  yellowText: '#7F7200',
+  blueBackground: '#CDFCFF',
+  blueText: '#00777F',
+  purpleBackground: '#DDCDFF',
+  purpleText: '#38007F',
+};
+
+export default colors;
